d6e6462c37cacbf71e45efc8dd947fc8e1e17035
Andres Lopez <andres@bakedweb.net>
2012-08-17 16:46:10 -0500

    Fold 3, graph redraw

--- public/js/efects.js
+++ public/js/efects.js
@@ -1,11 +1,58 @@
 $(document).ready(function(cuenta) {
-	var scrollorama = $.scrollorama({blocks:'#wrapper', enablePin:false});
-	scrollorama.animate('.fold_quees .quees',{
+	$scrollorama = $.scrollorama({blocks:'#wrapper', enablePin:false});
+	$scrollorama.animate('.fold_quees .quees',{
 		duration: 770, property:'zoom', start: 0, end: 1},{
 		duration: 800, property:'opacity', start: 0, end: 1}
 	);
-
-
+	/*PERCENT*/
+	$next = "#graph_1";
+	$coming = graph1();
+	function time() {
+		$('.fold_inboundoutbound .image').fadeOut('slow');
+		$coming();
+	}
+	function graph1() {
+		$($next).delay(600).fadeIn('slow');
+		setTimeout(function(){
+			$next = "#graph_2";
+			$coming = graph2;
+			time();
+		}, 5000);
+		
+	}
+	function graph2() {
+		$($next).delay(600).fadeIn('slow');
+		setTimeout(function(){
+			$next = "#graph_3";
+			$coming = graph3;
+			time();
+		}, 5000);
+	}
+	function graph3() {
+		$($next).delay(600).fadeIn('slow');
+		setTimeout(function(){
+			$next = "#graph_4";
+			$coming = graph4;
+			time();
+		}, 5000);
+	}
+	function graph4() {
+		$($next).delay(600).fadeIn('slow');
+		setTimeout(function(){
+			$next = "#graph_5";
+			$coming = graph5;
+			time();
+		}, 5000);
+	}
+	function graph5() {
+		$($next).delay(600).fadeIn('slow');
+		setTimeout(function(){
+			$next = "#graph_1";
+			$coming = graph1;
+			time();
+		}, 5000);
+	}
+	
 	/*HOW*/
 	$('#clave').click(function(){
 		$('.description').hide();
