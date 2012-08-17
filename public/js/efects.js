5ec3ab218bfaacc3d16f9c0d5d459596d63b1310
Andres Lopez <andres@bakedweb.net>
2012-08-17 11:19:04 -0500

    efecto en segundo fold

--- public/js/efects.js
+++ public/js/efects.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function(cuenta) {
 	var scrollorama = $.scrollorama({blocks:'.fold_quees', enablePin:false});
 	scrollorama.animate('.fold_quees .quees',{
-		duration: 150, property:'opacity', start:0, end:1}
+		duration: 200, property:'zoom', start: 0, end: 1},{
+		duration: 200, property:'opacity', start: 0, end: 1}
 	);
+
 
 	/*HOW*/
 	$('#clave').click(function(){
--- public/js/scroll_bubble.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var scrollorama = $.scrollorama({
-        blocks:'.fold_quees', enablePin:false
-    });
-    scrollorama.animate('.fold_quees .quees',{
-    duration: 150, property:'opacity', start:0, end:1
-    });
-});
